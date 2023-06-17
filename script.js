7047334bdb8393772a18fe111d894feecaf4a06c
Chamano Ramashia <chamano.sr@gmail.com>
2023-06-17 23:34:00 +0200

    update

--- script.js
+++ script.js
@@ -124,3 +124,17 @@ function checkPalindromic(str) {
 }
 
 checkPalindromic("madam")
+
+/*
+9. Write a function that takes two arrays as input and returns
+ a new array that contains only the elements that are present in both arrays.
+*/
+function findCommonElements(arr1, arr2) {
+  return arr1.filter(element => arr2.includes(element));
+}
+
+// Example usage
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7];
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [4, 5]
