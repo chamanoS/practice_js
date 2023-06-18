95110546027705762aca46b7ab8c6160ead9bbdd
Chamano Ramashia <chamano.sr@gmail.com>
2023-06-18 14:09:36 +0200

    avg sum

--- math.js
+++ math.js
@@ -4,10 +4,24 @@
 
 function sumNumbers() {
     let sum = 0;
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 50; i++) {
       sum += i;
     }
     return sum;
   }
   
 console.log(sumNumbers());
+
+/*
+3. Given an array of numbers, write a function to calculate the average value.
+*/
+
+function averageValue(arr){
+    let sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) { 
+             sum += arr[i];
+    }
+    return sum/ arr.length ;
+}
+
